a0938245102ce5597d87a887eb00f5701980582a
Nick Jaensson <n.jaensson@gmail.com>
2023-05-03 13:00:03 +0200

    Simplify calculation of von Mises stress

--- von_Mises.m
+++ von_Mises.m
@@ -1,15 +1,9 @@
 % function to calculate von Mises stresses
 function [taud] = von_Mises(svec)
 
-    % unpack the vector
-    sxx = svec(1); sxy = svec(2); sxz = svec(3); 
-    syy = svec(4); syz = svec(5); szz = svec(6);
-
-    % put the 6 values in a 3x3 symmetrix matrix
-    ss = [sxx,sxy,sxz;sxy,syy,syz;sxz,syz,szz];
-
-    % determine von Mises stresses
-    sshat = ss - (1/3)*trace(ss)*eye(3);
-    taud = sqrt(0.5*(trace(sshat*sshat)));
+    taud = sqrt ( ( ( svec(1) - svec(4) ) ^ 2 + ...
+                    ( svec(4) - svec(6) ) ^ 2 + ...
+                    ( svec(6) - svec(1) ) ^ 2 ) / 6 + ...
+                      svec(2) ^ 2 + svec(3) ^ 2 + svec(5) ^ 2 );
 
 end
